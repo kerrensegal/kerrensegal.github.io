98a529f1602d620ac75089b2a2f3708fc1854654
Kerren Segal <ksegal@solvista.com>
2023-09-25 16:00:26 -0400

    updates

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from . import util
-from django import forms
+from django import forms, redirect
+from django.contrib import messages
 from markdown2 import Markdown
 
 
 class NewEntryForm(forms.Form):
+    title = forms.CharField(label="New Entry")
+    textarea = forms.CharField(label="Text")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
@@ -22,17 +25,73 @@ def entry(request, title):
         "page": Markdown().convert(page)
     })
 
-def search(request):
+# def search(request):
 #     query = request.GET.get("q", "")
 
 def new(request):
-    if request.method == "GET":
-        return render(request, "encyclopedia/new.html", {"form": NewEntryForm()})
     
-    # ******* FINISH WRITING CODE *************
-    
-def edit(request, title):
-    # ******* FINISH WRITING CODE *************
+    # Route reached via POST
+    if request.method == "POST":        
+        
+        # Form to submit a new entry
+        form = NewEntryForm(request.POST)
 
-def random(request):
+        # Checks if form is valid
+        if form.is_valid():
+            
+            # Form includes a title and text for the new entry
+            title = form.cleaned_data["title"]
+            textarea = form.cleaned_data["textarea"]
+
+            # Checks whether entry title already exists
+            if title.lower() in [entry.lower() for entry in util.list_entries()]:
+                messages.add_message(request, messages.WARNING, message=f'"{title}" already exists.')
+
+            else:
+
+                # Creates new file for the submitted entry
+                with open(f"entries/{title}.md", "w") as file:
+                    file.write(textarea)
+
+                # Redirects user to the new entry's page
+                return redirect("entry", title)    
+        
+        # If form is invalid
+        else:
+            return render(request, "encyclopedia/new.html", {"form": form})
+
+    # Route reached via GET
+    return render(request, "encyclopedia/new.html", {"form": NewEntryForm()}) 
+    
+def edit(request, entry):
+
+    # Route reached via POST
+    if request.method == "POST":
+
+        # Form to submit entry
+        form = NewEntryForm(request.POST)
+
+        # Checks if form is valid
+        if form.is_valid():
+
+            # Form includes a title and text for the entry
+            title = form.cleaned_data["title"]
+            textarea = form.cleaned_data["textarea"]
+
+            # Saves entry
+            util.save_entry(title=title, textarea=textarea)
+
+            # Redirects user to the entry's page
+            return redirect("entry", title)
+        
+    # Route reached via GET
+    title = entry
+    textarea = util.get_entry(title)
+    form = NewEntryForm({"title": title, "textarea": textarea})
+    return render(request, "encyclopedia/edit.html", {
+        "form": form,
+        "title": title
+    })
+
+# def random(request):
     # ******* FINISH WRITING CODE *************

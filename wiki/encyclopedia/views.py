368b18d1d63944e7334dc4878c4c8b82d589fe21
Kerren Segal <ksegal@solvista.com>
2023-09-25 14:26:09 -0400

    progress

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
-
 from . import util
+from django import forms
+from markdown2 import Markdown
 
+
+class NewEntryForm(forms.Form):
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
+
+def entry(request, title):
+    
+    # Check if requested entry title exists
+    if title not in util.list_entries():
+        return render("error.html")
+    
+    # Render the entry's page content
+    page = util.get_entry(title)
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "page": Markdown().convert(page)
     })
 
+def search(request):
+#     query = request.GET.get("q", "")
+
+def new(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html", {"form": NewEntryForm()})
+    
+    # ******* FINISH WRITING CODE *************
+    
+def edit(request, title):
+    # ******* FINISH WRITING CODE *************
+
+def random(request):
+    # ******* FINISH WRITING CODE *************

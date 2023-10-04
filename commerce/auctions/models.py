75386d0bfb55b905b54920a9f4b0076c1c8bef43
Kerren Segal <ksegal@solvista.com>
2023-10-04 16:32:26 -0400

    progress

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+categories = [
+    ('fashion', 'Fashion'),
+    ('toys', 'Toys'),
+    ('electronics', 'Electronics'),
+    ('home', 'Home'),
+    ('sports', 'Sports'),
+    ('pets', 'Pets'),
+    ('books', 'Books'),
+    ('music', 'Music'),
+    ('art', 'Art'),
+    ('health', 'Health'),
+    ('business', 'Business')
+]
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=500)
+    starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.CharField(max_length=20, choices=categories, blank=True)
+    image = models.URLField(blank=True)
+
+# class Bid():
+
+# class Comment():
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,8 +3,14 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, Listing
+
+class NewListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ('title', 'description', 'starting_bid', 'category', 'image')
 
 
 def index(request):
@@ -61,3 +67,40 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+    
+def create(request):
+
+    # Route reached via POST
+    if request.method == "POST":
+        
+        # Form to create a new listing
+        form = NewListingForm(request.POST)
+
+        # Checks if form is valid
+        if form.is_valid():
+
+            # Form includes title, description, starting bid, and optional category/image
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            starting_bid = form.cleaned_data["starting_bid"]
+            category = form.cleaned_data["category"]
+            image = form.cleaned_data["image"]
+
+            # Creates new listing
+            listing = Listing.objects.create_listing(title, description, starting_bid, category, image)
+            listing.save()
+
+            # Redirect user to the new listing
+            return HttpResponseRedirect(reverse("listing", args=(title,)))
+        
+        # If form is invalid
+        else:
+            return render(request, "auctions/create.html", {"form": form})
+    
+    # Route reached via GET
+    return render(request, "auctions/create.html", {"form": NewListingForm()})
+
+
+def listing(request):
+    return render(request, "auctions/listing.html")
